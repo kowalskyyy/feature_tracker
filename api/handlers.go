73b8813f53b953084b41447dcc8e467899936ee1
Bartosz Kowalski <bartosz.kowalski@pinmeto.com>
2024-05-29 17:40:53 +0200

    add storing to mongodb functionality

--- api/handlers.go
+++ api/handlers.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	context_ "context"
 	"net/http"
+
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kowalskyyy/feature_tracker.git/internal"
@@ -18,6 +21,19 @@ func getData(client *mongo.Client) gin.HandlerFunc {
 		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Your order data is incorrect"})
 		return
 	}
+
+	collection := client.Database("FeatureTracker").Collection("Features")
+
+        // Create a context with a 10-second timeout
+        ctx, cancel := context_.WithTimeout(context_.Background(), 10*time.Second)
+        defer cancel() // Ensure the context is canceled to free resources
+
+        // Insert the item into the collection using the context
+        _, err := collection.InsertOne(ctx, data)
+        if err != nil {
+			context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Couldnt save data to mongodb", "error": err})
+            return
+        }
 
 
 
