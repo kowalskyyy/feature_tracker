d49b5125939dc5ac023c57178a9d4867685566a9
Bartosz Kowalski <bartosz.kowalski@pinmeto.com>
2024-05-30 14:53:48 +0200

    serve static html

--- api/routes.go
+++ api/routes.go
@@ -1,12 +1,18 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(router *gin.Engine) {
+	router.LoadHTMLGlob("templates/*")
 	router.POST("/submit-feature", submitFeature)
 	router.POST("/get-features-filter", getFeaturesWithFilter)
 	router.POST("/get-features-query", getFeaturesWithQuery)
-
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})})
 }
